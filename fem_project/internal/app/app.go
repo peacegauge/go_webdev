6aafaf5e37eed501e22eb31651c675d5ecaa317d
peacegauge <ezramarka@gmail.com>
2025-04-27 08:12:20 +0200

    add sql add and update workout queries

--- fem_project/internal/app/app.go
+++ fem_project/internal/app/app.go
@@ -32,9 +32,10 @@ func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
+	workoutStore := store.NewPostgresWorkoutStore(pgDB)
 
 	// our handlers will go here
-	workoutHandler := api.NewWorkoutHandler()
+	workoutHandler := api.NewWorkoutHandler(workoutStore)
 
 	app := &Application{
 		Logger:         logger,
